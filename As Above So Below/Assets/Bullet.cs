23b41a47ce62834c1f547a6a05654a656a518e55
ShayneEvans <evansshayne3@gmail.com>
2021-03-26 12:16:11 -0400

    Code Changes/Combat/Different Assets

---
+++ As Above So Below/Assets/Bullet.cs
@@ -0,0 +1,37 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public int damage = 20;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+
+
+    }
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        Destroy(gameObject);
+    }
+}
