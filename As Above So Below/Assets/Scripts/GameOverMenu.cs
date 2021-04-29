c49d38b4b6371f9d82c6be626d40a2708df31159
Logan Prestigiacomo <loganp33@icloud.com>
2021-04-29 14:08:51 -0400

    Game Over Menu Added (Still need to activate when player health = 0)

---
+++ As Above So Below/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu() {
+        //Load previous scene --> The previous scene should be the Main Menu Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
